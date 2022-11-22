e516bfb31ca2175d94f370cf408c4182923ab7c0
RcleydsonR <rafael.cleydson@gmail.com>
2022-11-22 20:02:40 -0300

    adding initial texts for search/home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,34 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h2 v-bind:hidden="true">Home View</h2>
+  <div class="page">
+    <div class="text-container">
+      <span class="text font-extrabold text-5xl max-w-[29rem]"
+        >Adquira crypto com apenas um Pix</span
+      >
+      <span class="text font-medium text-base max-w-[28rem]"
+        >Digite um valor, selecione uma oferta, conecta sua carteira e receba a
+        crypto após realizar o Pix</span
+      >
+    </div>
+    <!-- <div
+      class="flex flex-col justify-center items-start px-8 py-6 gap-2 rounded-lg shadow-current backdrop-blur-md w-96 mt-10"
+    >
+    </div> 
+    Search component
+  --></div>
 </template>
+
+<style scoped>
+.page {
+  @apply flex flex-col items-center justify-center w-full mt-20;
+}
+
+.text-container {
+  @apply flex flex-col items-center justify-center gap-4;
+}
+
+.text {
+  @apply text-gray-800 text-center;
+}
+</style>

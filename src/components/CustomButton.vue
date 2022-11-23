fd6bace382b6d6d7d6ec4c9a7ffbf65aa11bf878
RcleydsonR <rafael.cleydson@gmail.com>
2022-11-23 18:07:18 -0300

    add custom button component
    Co-authored-by: Esio Freitas <esio.gustavo@gmail.com>

---
+++ src/components/CustomButton.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const props = defineProps({
+  text: String,
+  isDisabled: Boolean
+})
+
+const emit = defineEmits(['clicked'])
+</script>
+
+<template>
+    <button type="button" class="button" @click="emit('clicked')" v-bind:class="{'opacity-70': isDisabled}" :disabled="isDisabled">
+        {{ text }}
+    </button>
+</template>
+
+<style scoped>
+.button {
+  @apply rounded-lg w-full text-base font-semibold text-gray-900 py-4 bg-amber-400;
+}
+</style>

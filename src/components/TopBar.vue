716cd909a6d8d8c7abe1c9331cbf807442c4d97e
Enzo Gabriel <eggqsaraiva@gmail.com>
2022-12-20 15:00:35 -0300

    Remove duplicated import

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -4,7 +4,6 @@ import { storeToRefs } from "pinia";
 import { useEtherStore } from "../store/ether";
 import { ref } from "vue";
 import blockchain from "../utils/blockchain";
-import router from "@/router";
 
 // Store reference
 const etherStore = useEtherStore();
@@ -46,27 +45,13 @@ const disconnectUser = () => {
 
 <template>
   <header>
-    <img
-      alt="P2Pix logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="75"
-      height="75"
-    />
+    <img alt="P2Pix logo" class="logo" src="@/assets/logo.svg" width="75" height="75" />
     <div class="flex gap-4 items-center">
-      <button
-        type="button"
-        class="default-button"
-        v-on:click="router.push('/seller')"
-      >
+      <button type="button" class="default-button" v-on:click="router.push('/seller')">
         Quero vender
       </button>
-      <button
-        type="button"
-        v-if="!walletAddress"
-        class="border-amber-500 border-2 rounded default-button"
-        @click="connectMetaMask()"
-      >
+      <button type="button" v-if="!walletAddress" class="border-amber-500 border-2 rounded default-button"
+        @click="connectMetaMask()">
         Conectar carteira
       </button>
       <div v-if="walletAddress" class="account-info">
@@ -76,55 +61,34 @@ const disconnectUser = () => {
           <img alt="Chevron Down" src="@/assets/chevronDown.svg" />
         </div>
         <div class="flex flex-col">
-          <div
-            class="top-bar-info"
-            @mouseover="menuToggle = true"
-            @mouseout="menuToggle = false"
-            :style="{
-              backgroundColor: menuToggle ? '#F9F9F9' : 'transparent',
-            }"
-          >
+          <div class="top-bar-info" @mouseover="menuToggle = true" @mouseout="menuToggle = false" :style="{
+            backgroundColor: menuToggle ? '#F9F9F9' : 'transparent',
+          }">
             <img alt="Account image" src="@/assets/account.svg" />
-            <span
-              class="default-text text-sm"
-              :style="{
-                color: menuToggle ? '#000000' : 'rgb(249 250 251)',
-              }"
-            >
+            <span class="default-text text-sm" :style="{
+              color: menuToggle ? '#000000' : 'rgb(249 250 251)',
+            }">
               {{ formatWalletAddress() }}
             </span>
             <img alt="Chevron Down" src="@/assets/chevronDown.svg" />
           </div>
-          <div
-            v-show="menuToggle"
-            class="mt-10 absolute w-full text-black"
-            @mouseover="menuToggle = true"
-            @mouseout="menuToggle = false"
-          >
+          <div v-show="menuToggle" class="mt-10 absolute w-full text-black" @mouseover="menuToggle = true"
+            @mouseout="menuToggle = false">
             <div class="pl-4 mt-2">
               <div class="bg-white rounded-md z-10">
-                <div
-                  class="menu-button px-4 rounded-md cursor-pointer"
-                  onclick="window.location='/bid_history'"
-                >
+                <div class="menu-button px-4 rounded-md cursor-pointer" onclick="window.location='/bid_history'">
                   <div class="py-4 text-end font-semibold text-xs">
                     Histórico de compras
                   </div>
                   <hr />
                 </div>
-                <div
-                  class="menu-button px-4 cursor-pointer"
-                  onclick="window.location='/manage_bids'"
-                >
+                <div class="menu-button px-4 cursor-pointer" onclick="window.location='/manage_bids'">
                   <div class="py-4 text-end font-semibold text-xs">
                     Gerenciar Ofertas
                   </div>
                   <hr />
                 </div>
-                <div
-                  class="menu-button px-4 py-1 rounded-md cursor-pointer"
-                  @click="disconnectUser"
-                >
+                <div class="menu-button px-4 py-1 rounded-md cursor-pointer" @click="disconnectUser">
                   <div class="py-3 text-end font-semibold text-xs">
                     Desconectar
                   </div>
@@ -152,6 +116,7 @@ header {
 .default-button {
   @apply px-4 py-2 rounded text-gray-50 font-semibold text-base;
 }
+
 .account-info {
   @apply flex items-center gap-6;
 }

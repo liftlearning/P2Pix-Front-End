d81580a1341f3dda377fd190c3652610eaa15bd4
RcleydsonR <rafael.cleydson@gmail.com>
2022-12-25 23:52:34 -0300

    add Listing Component inside manage bids view

--- src/views/ManageBidsView.vue
+++ src/views/ManageBidsView.vue
@@ -1,12 +1,31 @@
 <script setup lang="ts">
+import { useEtherStore } from "@/store/ether";
+import { storeToRefs } from "pinia";
+import ListingComponent from "@/components/ListingComponent.vue";
+
+const etherStore = useEtherStore();
+const { depositsAddedList } = storeToRefs(etherStore);
 </script>
 
 <template>
-  <div class="page">Gerenciar Ofertas</div>
+  <div class="page">
+    <div class="header">
+      Gerenciar ofertas
+    </div>
+    <ListingComponent
+      v-if="depositsAddedList?.length != 0"
+      :wallet-transactions="depositsAddedList!"
+      :is-manage-mode="true"
+    ></ListingComponent>
+  </div>
 </template>
 
 <style scoped>
 .page {
-  @apply flex gap-8 mt-24;
+  @apply flex flex-col gap-10 mt-20 w-full;
+}
+
+.header{
+  @apply text-3xl text-gray-900 leading-9 font-bold justify-center flex
 }
 </style>

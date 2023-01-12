411966f7271258695b90818d5791c4e2ecba5d77
enzoggqs <eggqsaraiva@gmail.com>
2023-01-12 16:02:09 -0300

    Add more tests to TopBar component

--- src/components/TopBar/__tests__/TopBar.spec.ts
+++ src/components/TopBar/__tests__/TopBar.spec.ts
@@ -1,16 +1,35 @@
 /* eslint-disable no-undef */
 import { shallowMount } from "@vue/test-utils";
 import TopBar from "../TopBar.vue";
-import { createApp } from "vue";
-import App from "../../../App.vue";
-import { createPinia } from "pinia";
+import { useEtherStore } from "../../../store/ether";
 
-const app = createApp(App);
-app.use(createPinia());
+import { createPinia, setActivePinia } from "pinia";
 
 describe("TopBar.vue", () => {
-  it("render component when called", () => {
+  beforeEach(() => {
+    setActivePinia(createPinia());
+  });
+
+  it("should render connect wallet button", () => {
     const wrapper = shallowMount(TopBar);
-    // expect(wrapper).toBeCalled();
+    expect(wrapper.html()).toContain("Conectar carteira");
+  });
+
+  it("should render button to change to seller view when in buyer screen", () => {
+    const wrapper = shallowMount(TopBar);
+    expect(wrapper.html()).toContain("Quero vender");
+  });
+
+  it("should render button to change to seller view when in buyer screen", () => {
+    const etherStore = useEtherStore();
+    etherStore.setSellerView(true);
+    const wrapper = shallowMount(TopBar);
+    expect(wrapper.html()).toContain("Quero comprar");
+  });
+
+  it("should render the P2Pix logo correctly", () => {
+    const wrapper = shallowMount(TopBar);
+    const img = wrapper.findAll(".logo");
+    expect(img.length).toBe(1);
   });
 });

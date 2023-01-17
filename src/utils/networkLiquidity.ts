2cbf475e098436afb09c0a70425a4fdd1d2c0f9a
RcleydsonR <rafael.cleydson@gmail.com>
2023-01-17 19:25:22 -0300

    Add verify network liquidity method
    Co-authored-by: brunoedcf <brest.dallacosta@outlook.com>

---
+++ src/utils/networkLiquidity.ts
@@ -0,0 +1,17 @@
+const verifyNetworkLiquidity = (tokenValue: number, walletAddress: string, validDepositList: any[]) => {
+    const element = validDepositList.find((element) => {
+        const remaining = element.remaining;
+        if (
+          tokenValue!! <= remaining &&
+          tokenValue!! != 0 &&
+          element.seller !== walletAddress
+        ) {
+          return true;
+        }
+        return false;
+      });
+
+    return element
+}
+
+export { verifyNetworkLiquidity }
